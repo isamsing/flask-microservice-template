e16063ded71628adf0eb16ea103aad4da192830a
samneets <samneets@ciena.com>
2019-08-18 14:40:11 -0400

    Flask RESTful micro-service template

--- app/run.py
+++ app/run.py
@@ -1,9 +1,15 @@
+import logging
 import sys
 
 from api.bootstrap import init
 from api.configuration import configurations
 
+configuration = configurations[sys.argv[1]]
+api = init(configuration())
+app = api.app
+
 if __name__ == '__main__':
-    configuration = configurations[sys.argv[1]]
-    api = init(configuration())
-    api.app.run()
+    try:
+        app.run(threaded=True, use_reloader=False)
+    except Exception as exp:
+        logging.error(exp)
